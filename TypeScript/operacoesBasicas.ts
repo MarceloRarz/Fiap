bcca918c4d9f3d88f982a5929e2c76faeb59008f
Marcelo Neves <marcelo.nsilva@iob.com.br>
2024-04-03 21:09:22 -0300

    Add Aula 5 de TypeScript

---
+++ TypeScript/operacoesBasicas.ts
@@ -0,0 +1,13 @@
+export namespace MatematicaBasica {
+
+    export function somar(a: number, b: number){
+        return a + b;
+    }
+    
+    export function subtrair(a: number, b: number){
+        return a - b;
+    }    
+
+}
+
+
